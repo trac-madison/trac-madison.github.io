3f20080ebfb84d4c6645b16405850a551a8cc8c5
TRAC Madison <204110861+trac-madison@users.noreply.github.com>
2025-03-20 00:58:39 -0500

    Add .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+
+  basePath: '/',
+
+  images: {
+    unoptimized: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
